75fcd8c42e22562605b6fbbba01bca631bccc825
chatsagnik <sagnik.chatterjee95@gmail.com>
2025-01-13 19:34:53 +0530

    put an install script

---
+++ backup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# The following line extracts packages that were manually installed from the AUR and puts them into the packagelist file.
+pacman -Qqet | grep -v "$(pacman -Qqm)" > ./packagelist
+# The following line installs packages from the file packagelist. This command must be run as sudo. 
+cat ./packagelist | xargs pacman -S --needed --noconfirm
+
+# For local backup, one can use the following commands:
+# pacman -S --needed --noconfirm $(pacman -Qqe | grep -v "$(pacman -Qqm)")
